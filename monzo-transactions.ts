bf87f57b23792f5e2a31c425228c3ad62dfcdb9f
Jack O'Hara <jack.ohara@infinityworks.com>
2022-01-09 18:49:59 +0000

    Add a ProjectionExpression to the query

--- monzo-transactions.ts
+++ monzo-transactions.ts
@@ -39,7 +39,8 @@ export async function getTransactions() {
         const result = await dbClient.query({
             TableName: "monzo-transactions",
             ExpressionAttributeValues: { ":v1": { S: "Jack O'Hara" } },
-            KeyConditionExpression: "acount_holder_name = :v1"
+            KeyConditionExpression: "acount_holder_name = :v1",
+            ProjectionExpression: "transaction_date"
         })
 
         console.log("Successfully queried items");
