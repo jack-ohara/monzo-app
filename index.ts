ac3f58576a217323d5d0faa3e5b8e6987fa460bc
Jack O'Hara <jack.ohara@infinityworks.com>
2022-01-10 09:52:12 +0000

    Add the code back in with the correct event type

--- index.ts
+++ index.ts
@@ -1,34 +1,34 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2 } from "aws-lambda";
 import { getTransactions, storeTransaction } from "./monzo-transactions";
 
-export async function handler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
+export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> {
     console.log("Successfully entered the function!")
     console.log(event)
 
-    // if (event.requestContext.http.method === 'POST') {
-    //     console.log('New transaction received')
+    if (event.httpMethod === 'POST') {
+        console.log('New transaction received')
 
-    //     await storeTransaction(JSON.parse(event.body))
+        await storeTransaction(JSON.parse(event.body))
 
-    //     return { statusCode: 200 }
-    // } else if (event.requestContext.http.method === 'GET') {
-    //     try {
-    //         const items = await getTransactions();
+        return { statusCode: 200 }
+    } else if (event.httpMethod === 'GET') {
+        try {
+            const items = await getTransactions();
 
-    //         return {
-    //             statusCode: 200,
-    //             body: JSON.stringify(items)
-    //         }
-    //     } catch (err) {
-    //         return {
-    //             statusCode: 400,
-    //             body: JSON.stringify(err)
-    //         }
-    //     }
-    // }
+            return {
+                statusCode: 200,
+                body: JSON.stringify(items)
+            }
+        } catch (err) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify(err)
+            }
+        }
+    }
 
-    // console.log(`${event.requestContext.http.method} request received`)
-    // console.log('Doing nothing and exiting...')
+    console.log(`${event.httpMethod} request received`)
+    console.log('Doing nothing and exiting...')
     return {
         statusCode: 200,
         body: JSON.stringify({hello: "world"})
